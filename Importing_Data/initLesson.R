e3f223749b68fa9e539670501911f99595c3caff
Simon Queenborough <simon.queenborough@yale.edu>
2017-10-23 13:31:06 -0400

    importing data lesson

---
+++ Importing_Data/initLesson.R
@@ -0,0 +1,21 @@
+# For compatibility with 2.2.21
+swirl_options(swirl_logging = TRUE)
+
+
+.get_course_path <- function(){
+  tryCatch(swirl:::swirl_courses_dir(),
+           error = function(c) {file.path(find.package("swirl"),"Courses")}
+  )
+}
+
+# Path to installed lesson
+lessonpath <- file.path(.get_course_path(), "fes720_Explore",
+                        "Importing_Data", "data/")
+
+
+# open a text editor to see the data
+viewData <- function(file1 = 'example1.txt'){
+  file.edit(paste0(lessonpath, file1))
+}
+
+
