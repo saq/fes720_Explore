6fcbff11e9dcc13a4387d379647e060546616d35
Simon Queenborough <simon.queenborough@yale.edu>
2017-11-02 23:31:28 -0400

    devices lesson initial cmmit

---
+++ Graphics_Devices/initLesson.R
@@ -0,0 +1,16 @@
+# For compatibility with 2.2.21
+swirl_options(swirl_logging = TRUE)
+
+
+.get_course_path <- function(){
+  tryCatch(swirl:::swirl_courses_dir(),
+           error = function(c) {file.path(find.package("swirl"),"Courses")}
+  )
+}
+
+# Path to installed lesson
+lessonpath <- file.path(.get_course_path(), "fes720_Explore",
+                        "Graphics_Devices")
+
+
+
